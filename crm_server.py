ed3f366b0f2d2367e61e9f00e899d3fa53666bde
AlexFes <alexeiruxur@gmail.com>
2020-10-05 08:25:41 +0300

    add mailer to crm

--- crm_server.py
+++ crm_server.py
@@ -44,6 +44,12 @@ def authenticate():
     return creds
 
 
+def send_standard_reply(to, subject, text):
+    with EmailClient(email_addr, email_passwd) as client:
+        client.send_mail(to, subject, text)
+        print('email sent to: {}\n subject: {}\n'.format(to, subject))
+
+
 def write_line(sheet, row, line):
     header_body = {
         "values": [line]
@@ -87,11 +93,13 @@ def get_table(inbox):
                     "subject": subject,
                     "date": str(date.date()),
                     "client_date": str(date.date()),
-                    "our_date": ""
+                    "our_date": "",
+                    "is_first_client_mail": True
                 }
             # Update line
             else:
                 table[sender[1]]["client_date"] = str(date.date())
+                table[sender[1]]["is_first_client_mail"] = False
 
         # Our mail
         else:
@@ -102,15 +110,18 @@ def get_table(inbox):
                     "subject": subject,
                     "date": str(date.date()),
                     "client_date": "",
-                    "our_date": str(date.date())
+                    "our_date": str(date.date()),
+                    "is_first_client_mail": False
                 }
             # Update line
             else:
                 table[to[1]]["our_date"] = str(date.date())
+                table[to[1]]["is_first_client_mail"] = False
 
     for key, value in table.items():
         result.append([value["date"], key, value["sender"],
-                       value["subject"], value["client_date"], value["our_date"]])
+                       value["subject"], value["client_date"],
+                       value["our_date"], value["is_first_client_mail"]])
 
     return result
 
@@ -119,6 +130,7 @@ def update_sheet(sheet, table):
     data = []
 
     for ind, line in enumerate(table):
+        print(line)
         data.append({
             "range": "B{}:D{}".format(ind + 3, ind + 3),
             "values": [line[:3]]
@@ -129,8 +141,16 @@ def update_sheet(sheet, table):
         })
         data.append({
             "range": "O{}:P{}".format(ind + 3, ind + 3),
-            "values": [line[4:]]
+            "values": [line[4:6]]
         })
+
+        try:
+            if line[6]:
+                send_standard_reply(line[1],
+                                    "SmartDec | Smart Contract Services",
+                                    "Hi " + line[2] + "! Great seeing you today. Looking forward to connecting soon!")
+        except Exception as e:
+            print('failed to send message to: {}\n{}'.format(line[1], e))
 
     body = {
         "valueInputOption": "RAW",
@@ -156,6 +176,7 @@ if __name__ == '__main__':
     write_line(spreadsheet, 1, header)
 
     # Start polling yandex mail
+    # start(spreadsheet)
     while True:
         start(spreadsheet)
         time.sleep(10)
